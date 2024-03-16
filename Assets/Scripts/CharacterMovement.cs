8f55b21dbece9225a9a1c18f2e940a2b1faf5bd2
eren <115615808+ErenAtasun@users.noreply.github.com>
2024-03-16 13:42:27 +0300

    FixCharacterMovementCod
    FixCharacterMovementCodScripts

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -7,34 +7,29 @@ public class CharacterMovement : MonoBehaviour
     public float velocity = 5f;
     public float jumpForce = 5f;
     public Rigidbody2D rb;
-    public Sprite sprite1; // Birinci sprite
-    public Sprite sprite2; // Ýkinci sprite
+    public Sprite sprite1; 
+    public Sprite sprite2; 
 
     bool jump = false;
-    bool canJump = true; // Zýplama yeteneði kontrolü
+    bool canJump = true; 
     void Start()
     {
-        // Oyun baþladýðýnda otomatik olarak 1. durumu ayarla
         GetComponent<SpriteRenderer>().sprite = sprite1;
         transform.localScale = new Vector3(1, 1, 1);
     }
     void Update()
     {
-        // 1 tuþu kontrolü
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            // Sprite'ý ve boyutu deðiþtir
             GetComponent<SpriteRenderer>().sprite = sprite1;
-            transform.localScale = new Vector3(1, 1, 1); // Orjinal boyuta dön
-            canJump = true; // Zýplama yeteneðini aç
+            transform.localScale = new Vector3(1, 1, 1); 
+            canJump = true;
         }
-        // 2 tuþu kontrolü
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            // Sprite'ý ve boyutu deðiþtir
             GetComponent<SpriteRenderer>().sprite = sprite2;
-            transform.localScale = new Vector3(0.5f, 0.5f, 1); // Boyutu yarýya indir
-            canJump = false; // Zýplama yeteneðini kapat
+            transform.localScale = new Vector3(0.5f, 0.5f, 1); 
+            canJump = false; 
         }
 
         float HorizontalMove = Input.GetAxis("Horizontal");
@@ -42,7 +37,6 @@ public class CharacterMovement : MonoBehaviour
         Vector2 move = new Vector2(HorizontalMove * velocity, rb.velocity.y);
         rb.velocity = move;
 
-        // Zýplama yeteneðine göre kontrol yap
         if (Input.GetButtonDown("Jump") && !jump && canJump)
         {
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
